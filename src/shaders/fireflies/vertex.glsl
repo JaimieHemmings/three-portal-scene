a46e9b9ca93240a5a0b66ad437e2b73cab2badf8
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-27 01:59:28 +0000

    Update vertex.glsl

--- src/shaders/fireflies/vertex.glsl
+++ src/shaders/fireflies/vertex.glsl
@@ -1,20 +1,29 @@
 uniform float uPixelRatio;
 uniform float uSize;
 uniform float uTime;
-
 attribute float aScale;
+
+// Random number generator
+float fastRandom(float st, float seed) {
+    return 0.1 + fract(sin(st * 12.9898 + seed * 78.233)) * 0.3;
+}
 
 void main()
 {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.x += sin(uTime + modelPosition.y * 20.0) * (aScale * 0.1);
-  modelPosition.y += sin(uTime + modelPosition.x * 100.0) * (aScale * 0.05);
-  modelPosition.z += sin(uTime + modelPosition.x * 100.0) * (aScale * 0.05);
-
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectedPosition = projectionMatrix * viewPosition;
-
-  gl_Position = projectedPosition;
-  gl_PointSize = uSize * uPixelRatio * aScale;
-  gl_PointSize *= (1.0 / -viewPosition.z);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    
+    float randomFactor = fastRandom(aScale, 0.002);
+    float timeModulatedX = sin(uTime + modelPosition.y * 20.0) * (randomFactor / 6.0);
+    float timeModulatedY = sin(uTime + modelPosition.x * 100.0) * (randomFactor / 5.0);
+    float timeModulatedZ = sin(uTime + modelPosition.x * 100.0) * (randomFactor / 8.0);
+    
+    modelPosition.x += timeModulatedX;
+    modelPosition.y += timeModulatedY;
+    modelPosition.z += timeModulatedZ;
+    
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+    
+    gl_PointSize = uSize * uPixelRatio * aScale / -viewPosition.z;
 }
